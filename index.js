7797c8c6b49887aa98fa077d87a072607def2cbe
Vincent <eohndk@gmail.com>
2024-07-30 15:31:01 -0700

    removed rewards point button in menu, and top text

--- index.js
+++ index.js
@@ -37,5 +37,33 @@ function updatePowerups() {
             const elem = document.getElementsByClassName("Layout-sc-1xcs6mc-0 ftDwiu rewards-list");
             elem[0].children[i].innerHTML = "";
         }
+
+        //Debug
+        updateTopMenu();
     }, 700);
+}
+
+// function getChildren(str) {
+//     let arr = [];
+//     const elem = document.getElementsByClassName(str);
+//     const btn = elem[0].children;
+//     for (let i = 0; i < btn.length; i++) {
+//         arr.push(btn[i]);
+//     }
+//     return arr;
+// }
+
+function updateTopMenu() {
+    //Setup Constants
+    const elem = document.getElementsByClassName("Layout-sc-1xcs6mc-0 gNStSQ");
+    const botRow = document.getElementsByClassName("Layout-sc-1xcs6mc-0 krMWyI");
+    
+    //Update Top Row
+    const kebab = elem[0].children[1];
+    kebab.innerHTML = "";
+    const topText = elem[0].children[0].children[0].children[0].children[0].children[1];
+    topText.outerHTML = "";
+
+    //Add Close Button
+    botRow[0].children[1].innerHTML = "";
 }
